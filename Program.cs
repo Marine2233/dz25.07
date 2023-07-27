aa45886a894ac963920d3098ec92ba3338ee041f
Marine <marine_ponomarenko@mail.ru>
2023-07-27 16:43:31 +0300

    add HW

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// Console.WriteLine("Введите кол-во чисел :");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// int count = 0;
+// for(int i = 0; i < m; i ++)
+// {
+// Console.WriteLine($"Введите число {i+1}");
+// int num =Convert.ToInt32(Console.ReadLine());
+
+// if (num > 0)
+// count++ ;
+// }
+// System.Console.WriteLine($"Количество положительных чисел - {count}");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+//  задаются пользователем.
+
+
+// Console.Write("k1 = ");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("b1 = ");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("k2 = ");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("b2 = ");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k1 * x + b1;
+
+// Console.WriteLine($"Координаты точки пересечения (x = {x}, y = {y})");
+
