211414710fca1b216f0a3542010a4175fef9e2f3
Marine <marine_ponomarenko@mail.ru>
2023-07-28 08:39:29 +0300

    + streeng

--- Program.cs
+++ Program.cs
@@ -41,3 +41,107 @@
 
 // Console.WriteLine($"Координаты точки пересечения (x = {x}, y = {y})");
 
+
+
+// Задача 47. Задайте двумерный массив размером m×n,
+//  заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// int rows = 3;
+// int columns = 2;
+// double[,] matrix = new double[rows,columns];
+// var random = new Random() ;  
+
+// for(int i = 0; i < matrix.GetLength(0) ; i++)  
+// {
+//     for(int j = 0; i < matrix.GetLength(1); i++)  
+//     {
+//         matrix[i,j] = random.NextDouble()*20;
+//         Console.Write(matrix[i,j]+" ");  
+//     }
+
+//     System.Console.WriteLine();
+// }
+// не выводит строки
+
+// Задача 50. Напишите программу, которая на вход принимает
+//  позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+//  или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// int rows = 4;
+// int columns = 4;
+// int [,] matrix = new int [rows,columns];  
+// var random = new Random();  
+
+// for (int i= 0; i < matrix.GetLength(0); i++ )
+// {
+//   for( int j = 0; j < matrix.GetLength(1); j++)
+//   {
+//     matrix[i,j] = random.Next(0,10);
+//     Console.Write(matrix[i,j]+" ");
+//   }
+
+// Console.WriteLine();
+//  }
+
+// Console.Write("Введите номер строки: ");
+// int rowNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите номер столбца: ");
+// int colNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (rowNumber > 0 && rowNumber <= rows && colNumber > 0 && colNumber <= columns)
+// Console.WriteLine(matrix[rowNumber - 1, colNumber - 1]);
+
+// else
+// Console.WriteLine("Такого числа в массиве нет");
+
+
+// Задача 52. Задайте двумерный массив из целых чисел.
+//  Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int rows = 3;
+// int columns = 4;
+// int [,] matrix = new int[rows,columns];
+// var random = new Random();  
+
+// for(int i = 0; i < matrix.GetLength(0); i++)
+// { for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j] = random.Next(0,10);
+//         System.Console.Write(matrix[i,j] +" ");
+//     }
+//    Console.WriteLine();
+// }
+
+// int sum;
+// double average;
+// int count = rows;
+
+// for (int j = 0; j < columns; j++)
+// {
+// sum = 0;
+
+// for(int i = 0; i  < rows; i++)
+//     {
+//        sum += matrix[i, j];
+//     }
+                
+//       average = (double)sum / count;
+//       Console.WriteLine($"Среднее арифметическое в столбце {j + 1}: " + average);
+// }
